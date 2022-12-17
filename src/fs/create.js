f7234f780156c096625c6d6c9f61d090dc7186c1
Jalolov Sultonali <jssalijalol@gmail.com>
2022-12-17 17:35:53 +0500

    feat: implement create.js logic

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { join, dirname } from "path";
+import { promises } from "fs";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const filename = fileURLToPath(import.meta.url);
+  const filePath = join(dirname(filename), "files", "fresh.txt");
+  try {
+    promises.lstat;
+    await promises.stat(filePath);
+  } catch (error) {
+    return await promises.writeFile(filePath, "I am fresh and young");
+  }
+  throw new Error("FS operation failed");
 };
 
 await create();
