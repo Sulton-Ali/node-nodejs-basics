11dca9e73db4f4af5dda55c63365d8a96f713309
Jalolov Sultonali <jssalijalol@gmail.com>
2022-12-17 19:49:29 +0500

    feat: implement delete.js logic

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { rm } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+import { isExist } from "../helpers/index.js";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const filename = fileURLToPath(import.meta.url);
+  const filePath = join(dirname(filename), "files", "fileToRemove.txt");
+  if (!(await isExist(filePath))) {
+    throw new Error("FS operation failed");
+  }
+  await rm(filePath);
 };
 
 await remove();
